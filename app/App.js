f2f3073d17e231df8cf86adf40b1961cc49c8091
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2023-04-15 14:25:12 +0530

    authentication added

--- app/App.js
+++ app/App.js
@@ -3,36 +3,50 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import Onboarding from './Additional/onBoarding';
+import { useEffect, useState } from 'react';
+import { auth } from './firebase';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const unregister = auth.onAuthStateChanged(exist => {
+      if (exist) setUser(exist);
+      else setUser(null);
+    });
+
+    return () => {
+      unregister();
+    };
+  }, [user]);
   return (
     <NavigationContainer>
       <Stack.Navigator options={{
         headerShown: false,
-      }}>
-        {/* <Stack.Screen
-          options={{
-            headerShown: false,
-          }}
-          name="Login"
-          component={LoginScreen}
-        /> */}
-        <Stack.Screen
-          name="Onboarding"
-          component={Onboarding}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            headerShown: false,
-          }}
-        />
+      }}>{user ?
+        (
+          <>
+            <Stack.Screen
+              name="Onboarding"
+              component={Onboarding}
+
+            /><Stack.Screen
+              options={{
+                headerShown: false,
+              }}
+              name="Login"
+              component={LoginScreen}
+            />
+
+          </>) : (<>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+
+            />
+          </>)}
+
       </Stack.Navigator>
     </NavigationContainer>
   );
